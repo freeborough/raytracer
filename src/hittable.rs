4700e0f7ffe3859376b280d84fbdd754104a6776
Andy Freeborough <andy@freeborough.com>
2025-01-07 15:09:57 +0000

    Added support for front/back face detection.

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,9 +1,23 @@
-use crate::{Point3,Vector3,Ray};
+use crate::{Point3, Ray, Vector3, dot};
 
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vector3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vector3) {
+        // Sets the hit record normal vector.
+        // NOTE: The paramter `outward_normal` is assumed to have unit length.
+        self.front_face = dot(r.direction(), outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
 }
 
 pub trait Hittable {
